864c9625dd3432bd603ad09f1c7ee9fc4e4ce53f
Selman Bozoklu <s.bzkl45@gmail.com>
2022-06-25 15:31:43 +0300

    25/06/2022selman

--- src/test/java/pages/MusteriPage.java
+++ src/test/java/pages/MusteriPage.java
@@ -1,4 +1,19 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class MusteriPage {
+
+    public MusteriPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "(//span[@class='text'])[7]")
+    public WebElement costumersButton;
+
+
 }
