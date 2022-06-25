e8b58f075e4d454a33ca9d6ea6546234955fd46c
my-yilmaz <myyilmaz046@gmail.com>
2022-06-25 14:16:59 +0300

    25.06.2022

--- src/test/java/pages/UrunlerPage.java
+++ src/test/java/pages/UrunlerPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class UrunlerPage {
 
     public UrunlerPage() {
@@ -37,4 +39,56 @@ public class UrunlerPage {
 
     @FindBy(xpath = "//input[@id='is_downloadable']")
     public WebElement downloadable;
+
+    @FindBy(xpath = "//input[@id='pro_title']")
+    public WebElement productTitle;
+
+    @FindBy(xpath = "//input[@id='regular_price']")
+    public WebElement productPrice;
+
+    @FindBy(xpath = "//img[@id='featured_img_display']")
+    public WebElement imgAdd1;
+
+    @FindBy(xpath = "//*[@id='menu-item-browse']")
+    public WebElement ortamKutuphanesi;
+
+    @FindBy(xpath = "(//li[@tabindex='0'])[1]")
+    public WebElement img1;
+
+    @FindBy(xpath = "//button[text()='Seç']")
+    public WebElement secButonu;
+
+    @FindBy(xpath = "//img[@id='gallery_img_gimage_0_display']")
+    public WebElement imgAdd2;
+
+    @FindBy(xpath = "(//li[@data-id='9475'])[2]")
+    public WebElement img2;
+
+    @FindBy(xpath = "(//button[text()='Add to Gallery'])[2]")
+    public WebElement addToGallery;
+
+    @FindBy(xpath = "//img[@id='featured_img_display']")
+    public WebElement imgDisplay;
+
+    @FindBy(xpath = "//iframe[@id='excerpt_ifr']")
+    public WebElement iframeShortDescription;
+
+    @FindBy(xpath = "//html[@lang='tr']")
+    public WebElement shortDescription;
+
+    @FindBy(xpath = "//iframe[@id='description_ifr']")
+    public WebElement iframeDescription;
+
+    @FindBy(xpath = "//html[@lang='tr']")
+    public WebElement description;
+
+    @FindBy(xpath = "//*[@id='product_cats_checklist']/li")
+    public  List<WebElement> categories;
+
+    @FindBy(xpath = "//*[@id='product_cats_checklist']/li/input")
+    public  List<WebElement> categoriesButton;
+
+
+
+
 }
